6ad5336d5e9f91fd682954d11033d0e719ee7d6d
Niccolò Lucozzi <73986399+NikLucoz@users.noreply.github.com>
2022-10-21 21:39:57 +0200

    Update main.java

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ double num1 = 0;
 double num2 = 0;
 
 num1 = getDouble("Inserisci un numero 1");
-num2 = getDouble("Inserisci un numero 2")
+num2 = getDouble("Inserisci un numero 2");
 
 r = num1 + num2;
 println("risultato: " + r);
